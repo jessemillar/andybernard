192b02b9578f3cbc4baad16d2cb107e1d46d4cbc
Jesse Millar <hellojessemillar@gmail.com>
2015-05-08 10:41:15 -0600

    Adding GIFs

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var slackbot = require('node-slackbot');
 
+var gifURL = 'http://www.jessemillar.com/andybernard/images/gifs/';
+var gifs = [gifURL + 'awkward.gif', gifURL + 'dancing.gif', gifURL + 'flying.gif', gifURL + 'lachrymose.gif', gifURL + 'nailed it.gif', gifURL + 'smug.gif'];
+var closingTime = 'https://open.spotify.com/track/1A5V1sxyCLpKJezp75tUXn';
+
 var bot = new slackbot('xoxb-4783098406-oickxKu4Lh1AOphmqJUfK4C0');
 
 bot.use(function(message, callback) {
     if ('message' == message.type) {
-        console.log(message.user + ' said: ' + message.text);
+        // console.log(message.user + ' said: ' + message.text);
 
-        bot.sendMessage(message.channel, 'Test');
+        console.log(gifs[Math.round(Math.random() * (gifs.length - 0))]);
+
+        bot.sendMessage(message.channel, gifs[Math.round(Math.random() * (gifs.length - 0))]); // The default response of a random GIF
     }
 
     callback();
