73dc3032ef17357e0173fea71aac54b1e34464ae
Jesse Millar <hellojessemillar@gmail.com>
2015-05-08 13:43:06 -0600

    Announcing when it's closing time

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var currentGIF = 0; // Show the GIFs in order
+
 var config = require('./config.js'),
     slackbot = require('node-slackbot'),
     needle = require('needle');
@@ -6,19 +8,15 @@ var gifURL = 'http://www.jessemillar.com/andybernard/images/gifs/',
     gifs = [gifURL + 'awkward.gif', gifURL + 'dancing.gif', gifURL + 'flying.gif', gifURL + 'lachrymose.gif', gifURL + 'nailed.gif', gifURL + 'smug.gif'],
     closingTime = 'https://open.spotify.com/track/1A5V1sxyCLpKJezp75tUXn';
 
-var andy = new slackbot(config.botToken);
+console.log('The current time is ' + new Date().getHours() + ':' + new Date.getMinutes());
 
-// needle.get('https://slack.com/api/channels.list?token=' + config.apiToken, function(error, response) {
-//     if (!error) {
-//         for (var i = 0; i < response.body.channels.length; i++) {
-//             if (response.body.channels[i].name == 'bottest') {
-//                 console.log('Sending');
-//                 andy.sendMessage(response.body.channels[i].id, 'Test');
-//                 return;
-//             }
-//         }
-//     }
-// });
+setInterval(function() { // Tell everyone when it's time to go home (at 5pm)
+    if (new Date().getHours() == 16 && new Date().getMinutes() == 59) {
+        needle.get('https://slack.com/api/chat.postMessage?token=' + config.apiToken + "&channel=%23general&text=It's%20closing%20time%2C%20everybody!%20https%3A%2F%2Fopen.spotify.com%2Ftrack%2F1A5V1sxyCLpKJezp75tUXn");
+    }
+}, 1000 * 60); // Check the time every minute
+
+var andy = new slackbot(config.botToken); // Respond to messages
 
 andy.use(function(message, callback) {
     if (message.type == 'message' && message.user) {
@@ -26,7 +24,14 @@ andy.use(function(message, callback) {
             if (!error) {
                 if (!response.body.user.is_bot) { // Make sure we're not reading back one of our own messages
                     console.log(response.body.user.real_name + ' said, "' + message.text + '"');
-                    andy.sendMessage(message.channel, gifs[Math.round(Math.random() * (gifs.length - 0))]); // The default response of a random GIF
+
+                    andy.sendMessage(message.channel, gifs[currentGIF]); // The default response of a random GIF
+
+                    if (currentGIF < gifs.length - 1) { // Show the GIFs in order
+                        currentGIF++;
+                    } else {
+                        currentGIF = 0;
+                    }
                 }
             }
         });
