a9d5918182ac35e56909f5cca91171e916299cfa
Jesse Millar <hellojessemillar@gmail.com>
2015-05-08 11:24:43 -0600

    Fixed bugs

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-var slackbot = require('node-slackbot');
+var config = require('./config.js'),
+    slackbot = require('node-slackbot'),
+    needle = require('needle');
 
-var gifURL = 'http://www.jessemillar.com/andybernard/images/gifs/';
-var gifs = [gifURL + 'awkward.gif', gifURL + 'dancing.gif', gifURL + 'flying.gif', gifURL + 'lachrymose.gif', gifURL + 'nailed.gif', gifURL + 'smug.gif'];
+var gifURL = 'http://www.jessemillar.com/andybernard/images/gifs/',
+    gifs = [gifURL + 'awkward.gif', gifURL + 'dancing.gif', gifURL + 'flying.gif', gifURL + 'lachrymose.gif', gifURL + 'nailed.gif', gifURL + 'smug.gif'],
+    closingTime = 'https://open.spotify.com/track/1A5V1sxyCLpKJezp75tUXn';
 
-var closingTime = 'https://open.spotify.com/track/1A5V1sxyCLpKJezp75tUXn';
+var andy = new slackbot(config.apiToken);
 
-var bot = new slackbot('xoxb-4783098406-oickxKu4Lh1AOphmqJUfK4C0');
+andy.use(function(message, callback) {
+    if (message.user && message.text) {
+        console.log(message.user + ' said, "' + message.text + '"');
+    }
 
-bot.use(function(message, callback) {
-    console.log(message);
-
-    if ('message' == message.type) {
-        // console.log(message.user + ' said: ' + message.text);
-
-        bot.sendMessage(message.channel, gifs[Math.round(Math.random() * (gifs.length - 0))]); // The default response of a random GIF
+    if (message.type == 'message' && message.user) {
+        needle.get('https://slack.com/api/users.info?token=' + config.apiToken + '&user=' + message.user, function(error, response) {
+            if (!error) {
+                if (!response.body.user.is_bot) {
+                    andy.sendMessage(message.channel, gifs[Math.round(Math.random() * (gifs.length - 0))]); // The default response of a random GIF
+                }
+            }
+        });
     }
 
     callback();
 });
 
-bot.connect();
+andy.connect();
